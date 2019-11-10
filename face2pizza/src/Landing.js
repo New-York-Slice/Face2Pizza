69d12818c86054114e2afdcac56f5069346e5b10
Hareem Bokhari <hbokhari98@gmail.com>
2019-11-10 06:30:08 -0500

    add landing

---
+++ face2pizza/src/Landing.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import './App.css';
+
+function Landing(props) {
+  return (
+  	<div>
+  		<div className="navbar"></div>
+  		<div className="logo"></div>
+  		<div className="centerText1">
+  			Face Your Pizza
+  		</div>
+  		<div className="centerText2">
+  			Face2Pizza uses revolutionary computer vision technology to create your own personalized pizza based on your current mood.
+  		</div>
+  		<div>
+    		<Button href="/upload" type="button" className="buttonRef">It's Pizza Time!</Button>
+    	</div>
+    </div>
+  );
+}
+
+export default Landing;
+
