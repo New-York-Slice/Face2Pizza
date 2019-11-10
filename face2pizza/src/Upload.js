026556d6d233cc84c270805f17f65f0e11c06dcc
Kevin Tam <kevin@kevintam.dev>
2019-11-10 02:36:48 -0500

    Fix JSON parsing

--- face2pizza/src/Upload.js
+++ face2pizza/src/Upload.js
@@ -21,74 +21,77 @@ class Upload extends React.Component {
   }
 
   submitPicture = (event) => {
-    const  fileType = this.state.picture['type'];
+    const fileType = this.state.picture['type'];
     const validImageTypes = ['image/gif', 'image/jpeg', 'image/png'];
     if (validImageTypes.includes(fileType)) {
 
-        this.state.success = true;
+      this.state.success = true;
 
-  //'use strict';
+      //'use strict';
 
-  const request = require('request');
+      const request = require('request');
 
-  // Replace <Subscription Key> with your valid subscription key.
-  const subscriptionKey = 'f0fdfc418d304f1bbed171db474cf17f';
+      // Replace <Subscription Key> with your valid subscription key.
+      const subscriptionKey = 'f0fdfc418d304f1bbed171db474cf17f';
 
-// You must use the same location in your REST call as you used to get your
-// subscription keys. For example, if you got your subscription keys from
-// westus, replace "westcentralus" in the URL below with "westus".
-  const uriBase = 'https://eastus.api.cognitive.microsoft.com/face/v1.0/detect';
+      // You must use the same location in your REST call as you used to get your
+      // subscription keys. For example, if you got your subscription keys from
+      // westus, replace "westcentralus" in the URL below with "westus".
+      const uriBase = 'https://eastus.api.cognitive.microsoft.com/face/v1.0/detect';
 
-  const imageUrl = 'https://cdn.newsapi.com.au/image/v1/d55a797470d890e2bdffc34d3885e6e8';
-    // Request parameters.
-  const params = {
-    'returnFaceId': 'true',
-    'returnFaceLandmarks': 'false',
-    'returnFaceAttributes': 
-        'emotion'
-  };
+      const imageUrl = 'https://cdn.newsapi.com.au/image/v1/d55a797470d890e2bdffc34d3885e6e8';
+      // Request parameters.
+      const params = {
+        'returnFaceId': 'true',
+        'returnFaceLandmarks': 'false',
+        'returnFaceAttributes': 'emotion'
+      };
 
-  const options = {
-    uri: uriBase,
-    qs: params,
-    body: '{"url": ' + '"' + imageUrl + '"}',
-    headers: {
-        'Content-Type': 'application/json',
-        'Ocp-Apim-Subscription-Key' : subscriptionKey
+      const options = {
+        uri: uriBase,
+        qs: params,
+        body: '{"url": ' + '"' + imageUrl + '"}',
+        headers: {
+          'Content-Type': 'application/json',
+          'Ocp-Apim-Subscription-Key': subscriptionKey
+        }
+      };
+
+      request.post(options, (error, response, body) => {
+        if (error) {
+          console.log('Error: ', error);
+          return;
+        }
+        let jsonResponse = JSON.parse(body, null, '  ');
+        alert("parsed json")
+        console.log('JSON Response\n');
+        console.log(jsonResponse);
+
+        this.setState.res = {
+          happy: `${jsonResponse[0].faceAttributes.emotion.happiness}`,
+        };
+        console.log("Happy state: " + `${jsonResponse[0].faceAttributes.emotion.happiness}`)
+      });
     }
-  };
-
-  request.post(options, (error, response, body) => {
-  if (error) {
-    console.log('Error: ', error);
-    return;
   }
-    let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
-    console.log('JSON Response\n');
-    console.log(jsonResponse);
-    this.setState.res = {
-      happy: `${jsonResponse[0].faceAttributes.emotion.happiness}`,
-    };
-  });
-}
-}
-render(){
-  return (
-    <React.Fragment>
-          <div class="form-group">
-            <label for="picture">Attach a picture of yourself</label>
-              <input 
-                  type="file" 
-                  class="form-control-file" 
-                  id="picture"            
-                  onChange={this.fileChanged}
-                  required
-                />
-            <button className="btn btn-primary" onClick={this.submitPicture}>Upload!</button>
-    </div>
-    </React.Fragment>
-  );
-}
+  
+  render() {
+    return (
+      <React.Fragment>
+      <div class="form-group">
+        <label for="picture">Attach a picture of yourself</label>
+          <input 
+              type="file" 
+              class="form-control-file" 
+              id="picture"            
+              onChange={this.fileChanged}
+              required
+            />
+        <button className="btn btn-primary" onClick={this.submitPicture}>Upload!</button>
+      </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Upload;
