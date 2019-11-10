8f7290eb43167733e7d1cb922ecc97ecd8d39953
Kevin Tam <kevin@kevintam.dev>
2019-11-10 14:37:08 -0500

    Update

--- face2pizza/src/Pizza.js
+++ face2pizza/src/Pizza.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import './App.css';
+import { S_IFDIR } from 'constants';
 
 function Pizza(props) {
+  const toppings = ['Bacon', 'Mushrooms', 'Anchovies', 'Pepperoni', 'Extra Cheese', 'Barbeque Chicken', 'Spinach', 'Chili Peppers', 'Sausage'];
+    const choices = toppings
+        .filter(() => Math.random() > .5)
+        .slice(0, 3)
+        .join(', ');
+
   return (
     <div>
       <div className="navbar"></div>
@@ -12,9 +19,9 @@ function Pizza(props) {
             <div className="centerText3">
               Based on your Mood we suggest the following Toppings:
             </div>
-          <div className="centerText4">
-             Bacon, Mushrooms and Anchovies
-      </div>
+           <div className="centerText4">
+              {choices}
+           </div>
     </div>
   );
 }
