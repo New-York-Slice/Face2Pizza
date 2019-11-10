59b4d728ecbd9f5a04bf2850e8ed49ead5c7875f
Hareem Bokhari <hbokhari98@gmail.com>
2019-11-10 06:32:24 -0500

    add landing

--- face2pizza/src/Landing.js
+++ face2pizza/src/Landing.js
@@ -4,18 +4,18 @@ import './App.css';
 
 function Landing(props) {
   return (
-  	<div>
-  		<div className="navbar"></div>
-  		<div className="logo"></div>
-  		<div className="centerText1">
-  			Face Your Pizza
-  		</div>
-  		<div className="centerText2">
-  			Face2Pizza uses revolutionary computer vision technology to create your own personalized pizza based on your current mood.
-  		</div>
-  		<div>
-    		<Button href="/upload" type="button" className="buttonRef">It's Pizza Time!</Button>
-    	</div>
+    <div>
+      <div className="navbar"></div>
+      <div className="logo"></div>
+      <div className="centerText1">
+        Face Your Pizza
+      </div>
+      <div className="centerText2">
+        Face2Pizza uses revolutionary computer vision technology to create your own personalized pizza based on your current mood.
+      </div>
+      <div>
+        <Button href="/upload" type="button" className="buttonRef">It's Pizza Time!</Button>
+      </div>
     </div>
   );
 }
